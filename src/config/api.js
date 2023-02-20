c56d4f1235b65710f8fbad4f23cb7d5588a7266c
MACBOOK <PRO@MOJAVEs-MacBook-Pro.local>
2023-02-20 23:55:43 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1/",
+    baseURL: "holyways-server-production.up.railway.app",
 })
 
 export const setAuthToken = (token) => {
